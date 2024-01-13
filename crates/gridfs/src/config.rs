1abe976297ef68022b39bc452056163cca602a76
Noel <cutie@floofy.dev>
2024-01-13 14:45:09 -0800

    add gridfs impl. again with better code

---
+++ crates/gridfs/src/config.rs
@@ -0,0 +1,94 @@
+// 🐻‍❄️🧶 remi-rs: Robust, and simple asynchronous Rust crate to handle storage-related communications with different storage providers
+// Copyright (c) 2022-2024 Noelware, LLC. <team@noelware.org>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+use mongodb::options::{GridFsBucketOptions, ReadConcern, SelectionCriteria, WriteConcern};
+
+#[deprecated(since = "0.5.0", note = "`GridfsStorageConfig` has been renamed to `StorageConfig`")]
+pub type GridfsStorageConfig = StorageConfig;
+
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct StorageConfig {
+    /// Specifies the [`SelectionCriteria`].
+    #[cfg_attr(
+        feature = "serde",
+        serde(
+            default,
+            serialize_with = "serialize_selection_criteria",
+            skip_serializing_if = "Option::is_none"
+        )
+    )]
+    pub selection_criteria: Option<SelectionCriteria>,
+
+    /// Specifies the [`WriteConcern`] for all level acknowledgment when writing
+    /// new documents into the GridFS datastore. Read the [`MongoDB` documentation](https://www.mongodb.com/docs/manual/reference/write-concern)
+    /// for more information.
+    #[cfg_attr(feature = "serde", serde(default, skip_serializing_if = "Option::is_none"))]
+    pub write_concern: Option<WriteConcern>,
+
+    /// Specifies the [`ReadConcern`] for isolation for when reading documents from the GridFS store. Read the
+    /// [`MongoDB` documentation](https://www.mongodb.com/docs/manual/reference/write-concern) for more information.
+    #[cfg_attr(feature = "serde", serde(default, skip_serializing_if = "Option::is_none"))]
+    pub read_concern: Option<ReadConcern>,
+
+    /// Chunk size (in bytes) used to break the user file into chunks.
+    #[cfg_attr(feature = "serde", serde(default, skip_serializing_if = "Option::is_none"))]
+    pub chunk_size: Option<u32>,
+
+    /// Bucket name that holds all the GridFS datastore blobs.
+    pub bucket: String,
+}
+
+impl From<StorageConfig> for GridFsBucketOptions {
+    fn from(value: StorageConfig) -> Self {
+        GridFsBucketOptions::builder()
+            .selection_criteria(value.selection_criteria)
+            .read_concern(value.read_concern)
+            .write_concern(value.write_concern)
+            .chunk_size_bytes(value.chunk_size)
+            .bucket_name(value.bucket)
+            .build()
+    }
+}
+
+#[cfg(feature = "serde")]
+fn serialize_selection_criteria<S: ::serde::ser::Serializer>(
+    value: &Option<SelectionCriteria>,
+    serializer: S,
+) -> Result<S::Ok, S::Error> {
+    use ::mongodb::options::ReadPreference;
+    use ::serde::{ser::Error, Serialize};
+
+    if let Some(value) = value {
+        if matches!(value, SelectionCriteria::Predicate(_)) {
+            return Err(S::Error::custom(
+                "cannot use `SelectionCriteria::Predicate` to be serialized",
+            ));
+        }
+
+        match value {
+            SelectionCriteria::ReadPreference(rp) => return ReadPreference::serialize(&rp, serializer),
+            _ => unimplemented!(),
+        }
+    }
+
+    serializer.serialize_none()
+}
---
+++ crates/gridfs/src/service.rs
@@ -0,0 +1,439 @@
+// 🐻‍❄️🧶 remi-rs: Robust, and simple asynchronous Rust crate to handle storage-related communications with different storage providers
+// Copyright (c) 2022-2024 Noelware, LLC. <team@noelware.org>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+use crate::StorageConfig;
+use async_trait::async_trait;
+use bytes::{Bytes, BytesMut};
+use futures_util::{AsyncWriteExt, StreamExt};
+use mongodb::{
+    bson::{doc, raw::ValueAccessErrorKind, Bson, RawDocument},
+    options::GridFsFindOptions,
+    Database, GridFsBucket,
+};
+use remi::{Blob, File, ListBlobsRequest, UploadRequest};
+use std::{io, path::Path};
+use tokio_util::{compat::FuturesAsyncReadCompatExt, io::ReaderStream};
+
+fn to_io_error(error: mongodb::error::Error) -> io::Error {
+    io::Error::new(io::ErrorKind::Other, error)
+}
+
+fn value_access_err_to_io_error(error: mongodb::bson::raw::ValueAccessError) -> io::Error {
+    match error.kind {
+        ValueAccessErrorKind::NotPresent => {
+            io::Error::new(io::ErrorKind::NotFound, format!("key [{}] was not found", error.key()))
+        }
+
+        ValueAccessErrorKind::UnexpectedType { expected, actual, .. } => io::Error::new(
+            io::ErrorKind::InvalidData,
+            format!(
+                "expected BSON type '{expected:?}', actual type for key [{}] is '{actual:?}'",
+                error.key()
+            ),
+        ),
+
+        ValueAccessErrorKind::InvalidBson(err) => io::Error::new(io::ErrorKind::Other, err),
+        _ => unimplemented!(
+            "`ValueAccessErrorKind` was unhandled, please report it: https://github.com/Noelware/remi-rs/issues/new"
+        ),
+    }
+}
+
+fn document_to_blob(bytes: Bytes, doc: &RawDocument) -> Result<File, io::Error> {
+    let filename = doc.get_str("filename").map_err(value_access_err_to_io_error)?;
+    let length = doc.get_i64("length").map_err(value_access_err_to_io_error)?;
+    let content_type = doc.get_str("contentType").map_err(value_access_err_to_io_error)?;
+    let created_at = doc.get_datetime("uploadDate").map_err(value_access_err_to_io_error)?;
+
+    Ok(File {
+        last_modified_at: None,
+        content_type: Some(content_type.to_owned()),
+        created_at: if created_at.timestamp_millis() < 0 {
+            #[cfg(feature = "tracing")]
+            ::tracing::warn!(remi.service = "gridfs", %filename, "`created_at` timestamp was negative");
+            None
+        } else {
+            Some(
+                u128::try_from(created_at.timestamp_millis())
+                    .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?,
+            )
+        },
+
+        is_symlink: false,
+        data: bytes,
+        name: filename.to_owned(),
+        path: format!("gridfs://{filename}"),
+        size: if length < 0 {
+            0
+        } else {
+            length
+                .try_into()
+                .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?
+        },
+    })
+}
+
+#[deprecated(
+    since = "0.5.0",
+    note = "`GridfsStorageService` has been renamed to `StorageService`"
+)]
+pub type GridfsStorageService = StorageService;
+
+#[derive(Debug, Clone)]
+pub struct StorageService(GridFsBucket);
+
+impl StorageService {
+    /// Creates a new [`StorageService`] which uses the [`StorageConfig`] as a way to create
+    /// the inner [`GridFsBucket`].
+    pub fn new(db: &Database, config: StorageConfig) -> StorageService {
+        let bucket = db.gridfs_bucket(Some(config.into()));
+        StorageService::with_bucket(bucket)
+    }
+
+    /// Uses a preconfigured [`GridFsBucket`] as the underlying bucket.
+    pub fn with_bucket(bucket: GridFsBucket) -> StorageService {
+        StorageService(bucket)
+    }
+}
+
+#[async_trait]
+impl remi::StorageService for StorageService {
+    const NAME: &'static str = "remi:gridfs";
+
+    #[cfg_attr(
+        feature = "tracing",
+        tracing::instrument(
+            name = "remi.gridfs.open",
+            skip_all,
+            fields(
+                remi.service = "gridfs",
+                path = %path.as_ref().display()
+            )
+        )
+    )]
+    async fn open<P: AsRef<Path> + Send>(&self, path: P) -> io::Result<Option<Bytes>> {
+        let path = path.as_ref();
+
+        #[cfg(feature = "tracing")]
+        ::tracing::info!(remi.service = "gridfs", file = %path.display(), "opening file");
+
+        #[cfg(feature = "log")]
+        ::log::info!("opening file [{}]", path.display());
+
+        // ensure that the `path` is utf-8 encoded, because I think
+        // MongoDB expects strings to be utf-8 encoded?
+        let path_str = path
+            .to_str()
+            .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "expected utf-8 encoded path string"))?;
+
+        let mut cursor = self
+            .0
+            .find(doc! { "filename": path_str }, GridFsFindOptions::default())
+            .await
+            .map_err(to_io_error)?;
+
+        let advanced = cursor.advance().await.map_err(to_io_error)?;
+        if !advanced {
+            #[cfg(feature = "tracing")]
+            ::tracing::warn!(
+                remi.service = "gridfs",
+                file = %path.display(),
+                "file doesn't exist in GridFS"
+            );
+
+            #[cfg(feature = "log")]
+            ::log::warn!("file [{}] doesn't exist in GridFS", path.display());
+
+            return Ok(None);
+        }
+
+        let doc = cursor.current();
+        let stream = self
+            .0
+            .open_download_stream(Bson::ObjectId(
+                doc.get_object_id("_id").map_err(value_access_err_to_io_error)?,
+            ))
+            .await
+            .map_err(to_io_error)?;
+
+        let mut bytes = BytesMut::new();
+        let mut reader = ReaderStream::new(stream.compat());
+        while let Some(raw) = reader.next().await {
+            match raw {
+                Ok(b) => bytes.extend(b),
+                Err(e) => return Err(e),
+            }
+        }
+
+        Ok(Some(bytes.into()))
+    }
+
+    #[cfg_attr(
+        feature = "tracing",
+        tracing::instrument(
+            name = "remi.gridfs.blob",
+            skip_all,
+            fields(
+                remi.service = "gridfs",
+                path = %path.as_ref().display()
+            )
+        )
+    )]
+    async fn blob<P: AsRef<Path> + Send>(&self, path: P) -> io::Result<Option<Blob>> {
+        let path = path.as_ref();
+        let Some(bytes) = self.open(path).await? else {
+            return Ok(None);
+        };
+
+        // .unwrap() is safe here since .open() validates if the path is a
+        // utf-8 string.
+        let path_str = path.to_str().unwrap();
+
+        #[cfg(feature = "tracing")]
+        ::tracing::info!(
+            remi.service = "gridfs",
+            file = %path.display(),
+            "getting file metadata for file"
+        );
+
+        #[cfg(feature = "log")]
+        ::log::info!("getting file metadata for file [{}]", path.display());
+
+        let mut cursor = self
+            .0
+            .find(
+                doc! {
+                    "filename": path_str,
+                },
+                GridFsFindOptions::default(),
+            )
+            .await
+            .map_err(to_io_error)?;
+
+        // has_advanced returns false if there is no entries that have that filename
+        let has_advanced = cursor.advance().await.map_err(to_io_error)?;
+        if !has_advanced {
+            #[cfg(feature = "tracing")]
+            ::tracing::warn!(remi.service = "gridfs", file = %path.display(), "file doesn't exist");
+
+            #[cfg(feature = "log")]
+            ::log::warn!("file [{}] doesn't exist", path.display());
+
+            return Ok(None);
+        }
+
+        let doc = cursor.current();
+        Ok(Some(Blob::File(document_to_blob(bytes, doc)?)))
+    }
+
+    #[cfg_attr(
+        feature = "tracing",
+        tracing::instrument(
+            name = "remi.gridfs.blobs",
+            skip_all,
+            fields(
+                remi.service = "gridfs"
+            )
+        )
+    )]
+    async fn blobs<P: AsRef<Path> + Send>(
+        &self,
+        path: Option<P>,
+        _request: Option<ListBlobsRequest>,
+    ) -> io::Result<Vec<Blob>> {
+        // TODO(@auguwu): support filtering files, for now we should probably
+        // heavily test this
+        #[allow(unused)]
+        if let Some(path) = path {
+            #[cfg(feature = "tracing")]
+            ::tracing::warn!(
+                remi.service = "gridfs",
+                file = %path.as_ref().display(),
+                "using blobs() with a given file name is not supported",
+            );
+
+            #[cfg(feature = "log")]
+            ::log::warn!(
+                "using blobs() with a given file name [{}] is not supported",
+                path.as_ref().display()
+            );
+
+            return Ok(vec![]);
+        }
+
+        let mut cursor = self
+            .0
+            .find(doc!(), GridFsFindOptions::default())
+            .await
+            .map_err(to_io_error)?;
+
+        let mut blobs = vec![];
+        while cursor.advance().await.map_err(to_io_error)? {
+            let doc = cursor.current();
+            let stream = self
+                .0
+                .open_download_stream(Bson::ObjectId(
+                    doc.get_object_id("_id").map_err(value_access_err_to_io_error)?,
+                ))
+                .await
+                .map_err(to_io_error)?;
+
+            let mut bytes = BytesMut::new();
+            let mut reader = ReaderStream::new(stream.compat());
+            while let Some(raw) = reader.next().await {
+                match raw {
+                    Ok(b) => bytes.extend(b),
+                    Err(e) => return Err(e),
+                }
+            }
+
+            match document_to_blob(bytes.into(), doc) {
+                Ok(blob) => blobs.push(Blob::File(blob)),
+
+                #[cfg(any(feature = "tracing", feature = "log"))]
+                Err(e) => {
+                    #[cfg(feature = "tracing")]
+                    ::tracing::error!(remi.service = "gridfs", error = %e, "unable to convert to a file");
+
+                    #[cfg(feature = "log")]
+                    ::log::error!("unable to convert to a file: {e}");
+                }
+
+                #[cfg(not(any(feature = "tracing", feature = "log")))]
+                Err(_e) => {}
+            }
+        }
+
+        Ok(blobs)
+    }
+
+    #[cfg_attr(
+        feature = "tracing",
+        tracing::instrument(
+            name = "remi.gridfs.delete",
+            skip_all,
+            fields(
+                remi.service = "gridfs",
+                path = %path.as_ref().display()
+            )
+        )
+    )]
+    async fn delete<P: AsRef<Path> + Send>(&self, path: P) -> io::Result<()> {
+        let path = path.as_ref();
+
+        #[cfg(feature = "tracing")]
+        ::tracing::info!(remi.service = "gridfs", file = %path.display(), "deleting file");
+
+        #[cfg(feature = "log")]
+        ::log::info!("deleting file [{}]", path.display());
+
+        // ensure that the `path` is utf-8 encoded, because I think
+        // MongoDB expects strings to be utf-8 encoded?
+        let path_str = path
+            .to_str()
+            .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "expected utf-8 encoded path string"))?;
+
+        let mut cursor = self
+            .0
+            .find(
+                doc! {
+                    "filename": path_str,
+                },
+                GridFsFindOptions::default(),
+            )
+            .await
+            .map_err(to_io_error)?;
+
+        // has_advanced returns false if there is no entries that have that filename
+        let has_advanced = cursor.advance().await.map_err(to_io_error)?;
+        if !has_advanced {
+            #[cfg(feature = "tracing")]
+            ::tracing::warn!(remi.service = "gridfs", file = %path.display(), "file doesn't exist");
+
+            #[cfg(feature = "log")]
+            ::log::warn!("file [{}] doesn't exist", path.display());
+
+            return Ok(());
+        }
+
+        let doc = cursor.current();
+        let oid = doc.get_object_id("_id").map_err(value_access_err_to_io_error)?;
+
+        self.0.delete(Bson::ObjectId(oid)).await.map_err(to_io_error)
+    }
+
+    #[cfg_attr(
+        feature = "tracing",
+        tracing::instrument(
+            name = "remi.gridfs.exists",
+            skip_all,
+            fields(
+                remi.service = "gridfs",
+                path = %path.as_ref().display()
+            )
+        )
+    )]
+    async fn exists<P: AsRef<Path> + Send>(&self, path: P) -> io::Result<bool> {
+        match self.open(path).await {
+            Ok(Some(_)) => Ok(true),
+            Ok(None) => Ok(false),
+            Err(e) => Err(e),
+        }
+    }
+
+    #[cfg_attr(
+        feature = "tracing",
+        tracing::instrument(
+            name = "remi.gridfs.blob",
+            skip_all,
+            fields(
+                remi.service = "gridfs",
+                path = %path.as_ref().display()
+            )
+        )
+    )]
+    async fn upload<P: AsRef<Path> + Send>(&self, path: P, options: UploadRequest) -> io::Result<()> {
+        let path = path.as_ref();
+
+        #[cfg(feature = "tracing")]
+        ::tracing::info!(
+            remi.service = "gridfs",
+            file = %path.display(),
+            "uploading file to GridFS..."
+        );
+
+        #[cfg(feature = "log")]
+        ::log::info!("uploading file [{}] to GridFS", path.display());
+
+        // ensure that the `path` is utf-8 encoded, because I think
+        // MongoDB expects strings to be utf-8 encoded?
+        let path_str = path
+            .to_str()
+            .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "expected utf-8 encoded path string"))?;
+
+        let mut stream = self.0.open_upload_stream(path_str, None);
+        stream.write_all(&options.data[..]).await?;
+        stream.close().await
+
+        // TODO(@auguwu): add metadata to document that was created and the given content type
+        // if one was supplied.
+    }
+}
